2d31f2cc3ac37297a391419618dae0821912e616
rudy <rudyarandia@hotmail.com>
2021-09-06 12:05:57 -0400

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Commands.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace CommandAPI.Models
+{
+  public class Command
+  {
+    [Key]
+    [Required]  
+    public int Id {get; set;}
+    [Required]
+    [MaxLength(250)]
+    public string HowTo {get; set;}
+    [required]
+    public string Platform {get; set;}
+    [required]
+    public string CommandLine {get; set;}
+  }
+}
